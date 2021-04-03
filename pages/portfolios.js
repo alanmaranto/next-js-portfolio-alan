cc3933ca6da66c0105de24b616b79acd741e0bfc
alan <alanarostegui@hotmail.com>
2021-04-03 13:43:24 -0600

    fetch posts

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -1,12 +1,33 @@
 import React from "react";
+import axios from "axios";
 import Layout from "../components/layouts/Layout";
 
-const Portfolio = () => {
+const Portfolios = ({ posts }) => {
+  console.log(posts);
+
+  const renderPosts = () => {
+    return posts.map((post) => <li key={post.id}>{post.title}</li>);
+  };
   return (
     <Layout>
       <div>Hola</div>
+      <ul>{renderPosts()}</ul>
     </Layout>
   );
 };
 
-export default Portfolio;
+Portfolios.getInitialProps = async () => {
+  let posts = [];
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    posts = response.data;
+  } catch (error) {
+    console.log(error.message);
+  }
+
+  return { posts: posts.slice(0, 10) };
+};
+
+export default Portfolios;
