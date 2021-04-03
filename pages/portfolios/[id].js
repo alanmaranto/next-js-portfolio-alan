bd70be892c9a97fb0bc62ed9421783745301af85
alan <alanarostegui@hotmail.com>
2021-04-03 14:26:42 -0600

    get by id

--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -1,15 +1,31 @@
 import React from "react";
+import axios from "axios";
 import { useRouter } from "next/router";
 import Layout from "../../components/layouts/Layout";
 
-const Portfolio = () => {
-  const { query } = useRouter();
+const Portfolio = ({ portfolio }) => {
   return (
     <Layout>
       <h1>I am Portfolio Page</h1>
-      <h2>{query.id}</h2>
+      <span>{portfolio.title}</span>
+      <p>{portfolio.body}</p>
+      <p>{portfolio.id}</p>
     </Layout>
   );
+};
+
+Portfolio.getInitialProps = async ({ query }) => {
+  let post = {};
+  try {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts/${query.id}`
+    );
+    post = response.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+
+  return { portfolio :post };
 };
 
 export default Portfolio;
