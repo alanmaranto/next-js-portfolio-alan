1e68120f08d8d20082f615d7396d19071876fe53
alan <alanarostegui@hotmail.com>
2021-04-04 14:01:57 -0500

    layout improvement

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,53 @@
 import React from "react";
-import Layout from "../components/layouts/Layout";
-import { Container, Button } from "reactstrap";
+import BaseLayout from "../components/layouts/Layout";
+import { Container, Row, Col } from "reactstrap";
 
-const Home = () => {
-  return (
-    <Layout>
-      <Container>
-        <Button color="danger">Danger</Button>
-        <div className="parent-class sibling-class">
-          <h1>Hi</h1>
-          <h2>2</h2>
-          <div className="child-class">Hello</div>
-          <div className="parent-class-title">Bye</div>
-          <div className="parent-class-date">Bye</div>
+class Index extends React.Component {
+  render() {
+    return (
+      <BaseLayout className="cover">
+        <div className="main-section">
+          <div className="background-image">
+            <img src="/images/background-index.png" />
+          </div>
+          <Container>
+            <Row>
+              <Col md="6">
+                <div className="hero-section">
+                  <div className={`flipper`}>
+                    <div className="back">
+                      <div className="hero-section-content">
+                        <h2> Full Stack Web Developer </h2>
+                        <div className="hero-section-content-intro">
+                          Have a look at my portfolio and job history.
+                        </div>
+                      </div>
+                      <img className="image" src="/images/section-1.png" />
+                      <div className="shadow-custom">
+                        <div className="shadow-inner"> </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Col>
+              <Col md="6" className="hero-welcome-wrapper">
+                <div className="hero-welcome-text">
+                  <h1>
+                    Welcome to the portfolio website of Filip Jerga. Get
+                    informed, collaborate and discover projects I was working on
+                    through the years!
+                  </h1>
+                </div>
+                <div className="hero-welcome-bio">
+                  <h1>Let's take a look on my work.</h1>
+                </div>
+              </Col>
+            </Row>
+          </Container>
         </div>
-      </Container>
-    </Layout>
-  );
-};
+      </BaseLayout>
+    );
+  }
+}
 
-export default Home;
+export default Index;
