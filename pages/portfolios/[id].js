a8b14e68594e5c64996bbc82e290aa19333ead15
alan <alanarostegui@hotmail.com>
2021-04-03 19:56:22 -0600

    remove unusual code

--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -1,6 +1,5 @@
 import React from "react";
 import axios from "axios";
-import { useRouter } from "next/router";
 import Layout from "../../components/layouts/Layout";
 
 const Portfolio = ({ portfolio }) => {
