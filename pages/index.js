e11cf0c5236e3f80532249b245eb7698f39de92f
alan <alanarostegui@hotmail.com>
2021-04-03 20:44:22 -0600

    add sass

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,13 @@ const Home = () => {
     <Layout>
       <Container>
         <Button color="danger">Danger</Button>
+        <div className="parent-class sibling-class">
+          <h1>Hi</h1>
+          <h2>2</h2>
+          <div className="child-class">Hello</div>
+          <div className="parent-class-title">Bye</div>
+          <div className="parent-class-date">Bye</div>
+        </div>
       </Container>
     </Layout>
   );
