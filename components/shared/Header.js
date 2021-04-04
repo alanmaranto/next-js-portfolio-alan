a0143a6a5358577528049feafdd0a076a4804e8e
alan <alanarostegui@hotmail.com>
2021-04-04 15:18:37 -0500

    enhancement(header): solved warning

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -1,23 +1,15 @@
-import React, { useState } from 'react';
-import Link from 'next/link';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem
-} from 'reactstrap';
+import React, { useState } from "react";
+import Link from "next/link";
+import { Collapse, Navbar, NavbarToggler, Nav, NavItem } from "reactstrap";
 
-
-const BsNavLink = props => {
+const BsNavLink = (props) => {
   const { href, title } = props;
   return (
     <Link href={href}>
       <a className="nav-link port-navbar-link">{title}</a>
     </Link>
-  )
-}
+  );
+};
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -29,35 +21,36 @@ const Header = () => {
         className="port-navbar port-default absolute"
         color="transparent"
         dark
-        expand="md">
-        <NavbarBrand>
+        expand="md"
+      >
+        <div className="navbar-brand">
           <Link href="/">
             <a className="port-navbar-brand">Alan Maranto</a>
           </Link>
-        </NavbarBrand>
+        </div>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
             <NavItem className="port-navbar-item">
-              <BsNavLink href="/" title="Home"/>
+              <BsNavLink href="/" title="Home" />
             </NavItem>
             <NavItem className="port-navbar-item">
-              <BsNavLink href="/about" title="About"/>
+              <BsNavLink href="/about" title="About" />
             </NavItem>
             <NavItem className="port-navbar-item">
-              <BsNavLink href="/portfolios" title="Portfolios"/>
+              <BsNavLink href="/portfolios" title="Portfolios" />
             </NavItem>
             <NavItem className="port-navbar-item">
-              <BsNavLink href="/blogs" title="Blogs"/>
+              <BsNavLink href="/blogs" title="Blogs" />
             </NavItem>
             <NavItem className="port-navbar-item">
-              <BsNavLink href="/cv" title="Cv"/>
+              <BsNavLink href="/cv" title="Cv" />
             </NavItem>
           </Nav>
         </Collapse>
       </Navbar>
     </div>
   );
-}
+};
 
 export default Header;
