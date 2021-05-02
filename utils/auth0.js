42fae37100a13fb0f9768b61172ae7ade15cf8a1
alan <alanarostegui@hotmail.com>
2021-05-02 13:21:15 -0500

    add env variables

--- utils/auth0.js
+++ utils/auth0.js
@@ -1,23 +1,23 @@
 import { initAuth0 } from "@auth0/nextjs-auth0";
 
 export default initAuth0({
-  domain: "",
-  clientId: "",
-  clientSecret: "",
-  scope: "",
-  redirectUri: "",
-  postLogoutRedirectUri: "",
+  domain: process.env.AUTO0_DOMAIN,
+  clientId: process.env.AUTO0_CLIENT_ID,
+  clientSecret: process.env.AUTO0_SECRET,
+  scope: "openid profile",
+  redirectUri: process.env.AUTO0_REDIRECT_URI,
+  postLogoutRedirectUri: process.env.AUTO0_POST_LOGOUT_REDIRECT_URI,
   session: {
-    cookieSecret: "",
-    cookieLifetime: 60 * 60 * 8,
+    cookieSecret: process.env.AUTH0_COOKIE_SECRET,
+    /*     cookieLifetime: 60 * 60 * 8,
     cookieDomain: "",
     cookieSameSite: "lax",
     storeIdToken: false,
     storeAccessToken: false,
-    storeRefreshToken: false,
+    storeRefreshToken: false, */
   },
-  oidcClient: {
+  /*   oidcClient: {
     httpTimeout: 2500,
     clockTolerance: 10000,
-  },
+  }, */
 });
